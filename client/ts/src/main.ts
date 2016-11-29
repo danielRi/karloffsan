b0577a7cd0588ca4145b861c124564f4b313b30b
Daniel Riegler <daniel.riegler87@googlemail.com>
2016-11-29 19:15:20 +0100

    minimal working example; guide in readme

---
+++ client/ts/src/main.ts
@@ -0,0 +1,27 @@
+/// <reference path=”../lib/phaser.d.ts” />
+
+
+class SimpleGame {
+
+    game: Phaser.Game;
+
+    constructor() {
+        this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', { preload: this.preload, create: this.create });
+    }
+
+    preload() {
+        this.game.load.image('logo', '../client/res/img/phaser_logo.jpg');
+    }
+
+    create() {
+        var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
+        logo.anchor.setTo(0.5, 0.5);
+    }
+
+}
+
+window.onload = () => {
+
+    var game = new SimpleGame();
+
+};
